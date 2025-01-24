0e489e528295dbd3d9361112f4df0a0e23da1da1
paarandika <paarandika@gmail.com>
2025-01-24 00:08:52 -0500

    Resolve the mapping copy issue

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="pandukabhaya",
-    version="1.0.0",
+    version="1.0.1",
     author="akuruAI",
     description="An ASCII to Unicode text converter for 'FM Abhaya' Sinhala font, with extensibility for other fonts via JSON mappings.", # noqa
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/akuruAI/Pandukabhaya",
-    packages=find_packages(exclude=["tests", "scripts"]),
     include_package_data=True,
+    packages=['pandukabhaya'],
+    package_dir={'pandukabhaya': 'pandukabhaya'},
+    package_data={'pandukabhaya': ['mappings/*.json']},
     install_requires=[],
     entry_points={
         "console_scripts": [
